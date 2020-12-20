c38e3231c03480df0eb3ca12388e48ffada39a12
JeffeSilva <jeffe.silva@yahoo.com>
2020-12-20 20:25:12 -0300

    Refazendo todos os exercicios, porem agora atraves do terminal com Tmux/Vim e usando git atraves dos comandos sem interface grafica somente terminal

---
+++ cap-2/Exercicio-2.1.py
@@ -0,0 +1,8 @@
+#Converta as seguintes espressões matemáticas para que possam ser calculadas usando o interpretador Python
+n1 = (10 + 20) * 30
+n2 = (4 ** 2) % 3
+n3 = (9 ** 4 + 2) * 6 - 1
+#Resultados
+print(f'O resultado da primeira equação é: {n1}')
+print(f'O resultado da segunda equação é: {n2}')
+print(f'O resultado da terceira equação é: {n3}')
---
+++ cap-2/Exercicio-2.2.py
@@ -0,0 +1,4 @@
+#Digite a seguinte expressão no interpretador:
+n1 = 10 % 3 * 10 ** 2 + 1 - 10 * 4 / 2
+#Resultado
+print(f'O resultado da expressão interpretada por python é: {n1}')
