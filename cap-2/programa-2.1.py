c77cd3fd3066453b1bd50f5e214a63fca8870bee
JeffeSilva <jeffe.silva@yahoo.com>
2020-12-20 23:00:01 -0300

    Programa 2.1 e 2.2 do livro

---
+++ cap-2/programa-2.1.py
@@ -0,0 +1,4 @@
+#Programa 2.1 - Seguindo o livro seria o Primeiro programa com variaveis
+a = 2
+b = 3
+print(a + b)
---
+++ cap-2/programa-2.2.py
@@ -0,0 +1,4 @@
+# Programa 2.2 - Calculo de aumento de salario
+salario = 1500
+aumento = 5
+print(salario + (salario * aumento / 100))
