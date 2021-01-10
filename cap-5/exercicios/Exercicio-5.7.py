50369a6195c42a06183fd27e9b1e88f0e2b0dd3b
JeffeSilva <jeffe.silva@yahoo.com>
2021-01-10 18:22:19 -0300

    Permissão de execução

--- cap-5/exercicios/Exercicio-5.7.py
+++ cap-5/exercicios/Exercicio-5.7.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
 # Exercicio 5.7 - Modifique o programa anterior de forma que o usuario tambem digite o inicio e o fim da tabuada, em vez de começar com 1 e 10.
-inicio = int(input('Digite o número que deseja começar: '))
-fim = int(input('Digite o número que deseja terminar: '))
-while inicio <= fim:
-    print('{} x {} = '.format(inicio, fim), (inicio * fim))
-    inicio += 1
+print(' ')
+print('{:=^35}'.format (' Tabuada '))
+tabuada = int(input('Tabuada do: '))
+fim = int(input('Até quanto deseja a tabuada: '))
+print('{:=^35}'.format (''))
+print(' ')
+cont = 1
+while cont <= fim:
+    print('{} x {:2} = '.format(tabuada, cont), (tabuada * cont))
+    cont += 1
+print(' ')
+print('{:=^35}'.format (' FIM '))
+print(' ')
